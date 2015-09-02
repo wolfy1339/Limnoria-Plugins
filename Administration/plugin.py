dd1a7c344db7cf2148615da3d8a7ac7084b26cbe
wolfy1339 <wolfy1339@users.noreply.github.com>
2015-09-02 18:21:13 -0400

    Make Administration plugin default

--- Administration/plugin.py
+++ Administration/plugin.py
@@ -2,7 +2,29 @@
 # Copyright (c) 2013-2015, wolfy1339
 # All rights reserved.
 #
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
 #
+#   * Redistributions of source code must retain the above copyright notice,
+#     this list of conditions, and the following disclaimer.
+#   * Redistributions in binary form must reproduce the above copyright notice,
+#     this list of conditions, and the following disclaimer in the
+#     documentation and/or other materials provided with the distribution.
+#   * Neither the name of the author of this software nor the name of
+#     contributors to this software may be used to endorse or promote products
+#     derived from this software without specific prior written consent.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
 ###
 
 import gc
@@ -21,9 +43,18 @@ try:
     from supybot.i18n import PluginInternationalization
     _ = PluginInternationalization('Administration')
 except:
-    # Placeholder that allows to run the plugin on a bot
-    # without the i18n module
     _ = lambda x:x
+
+def registerDefaultPlugin(command, plugin):
+    command = callbacks.canonicalName(command)
+    conf.registerGlobalValue(conf.supybot.commands.defaultPlugins,
+                             command, registry.String(plugin, ''))
+    # This must be set, or the quotes won't be removed.
+    conf.supybot.commands.defaultPlugins.get(command).set(plugin)
+
+registerDefaultPlugin('reload', 'Administration')
+registerDefaultPlugin('enable', 'Administration')
+registerDefaultPlugin('disable', 'Administration')
 
 class Administration(callbacks.Plugin):
     """Clone of the stock Owner plugin on Limnoria that works for Admin level too"""
