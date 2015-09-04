2d846b0d910ecdb774b01c9550c06894aafc6c31
wolfy1339 <wolfy1339@users.noreply.github.com>
2015-09-04 13:12:45 -0400

    Multiple Updates
    - Re-indent with 4 spaces
    - Fix self._getUserInfo()
    - Start adding logic to profile()

--- UserInfo/plugin.py
+++ UserInfo/plugin.py
@@ -36,20 +36,20 @@ import supybot.callbacks as callbacks
 import json
 
 try:
-	from supybot.i18n import PluginInternationalization
-	_ = PluginInternationalization('UserInfo')
+    from supybot.i18n import PluginInternationalization
+    _ = PluginInternationalization('UserInfo')
 except ImportError:
-	_ = lambda x:x
+    _ = lambda x:x
 
 class UserInfo(callbacks.Plugin):
-	"""A plugin that fetches member information from the BMN website"""
-	threaded = True
-	def profile(self, irc, msg, args, user):
-		"""<memberName>
+    """A plugin that fetches member information from the BMN website"""
+    threaded = True
+    def profile(self, irc, msg, args, user):
+        """<memberName>
 
-		Returns user information from their record"""
-		self._getMemberInfo(irc, msg, user, 0)
-	profile = wrap(profile,['somethingWithoutSpaces'])
+        Returns user information from their record"""
+        self._getMemberInfo(self, irc, user)
+    profile = wrap(profile,['somethingWithoutSpaces'])
 
     def UserInfoSnarfer(self, irc, msg, match):
         r"http://brilliant-minds.tk/members.html\?([a-zA-Z0-9_])|@([a-zA-Z0-9_])"
@@ -61,7 +61,7 @@ class UserInfo(callbacks.Plugin):
         if self.registryValue('MemberSnarfer') == "False":
             return
         else:
-            self._getMemberInfo(irc, msg, Name)
+            self._getMemberInfo(self, irc, Name)
 
     UserInfoSnarfer = urlSnarfer(UserInfoSnarfer)
 
@@ -70,65 +70,80 @@ class UserInfo(callbacks.Plugin):
 
         Returns the current members list"""
         data = json.loads(utils.web.getUrl("http://brilliant-minds.tk/members.json"))
+        officers = data["officers"]
+        enlisted = data["enlisted"]
+        preofficers = data["preofficers"]
+ 
+        for member, rank in officers:
+
+        for members, rank in enlisted:
+  
+        for members, rqank in preofficers:
+
+        if irc.channel != "#BMN":
+            irc.queueMsg(ircmsgs.privmsg(msg.nick, "{0}".format(data))
+        else:
+            irc.reply("{0}".format(data), nickPrefix=false)
     members = wrap(members)
 
-	def _getMemberInfo(self, irc, msg, user):
-		"""<username>
+    def _getMemberInfo(self, irc, user):
+        """<username>
 
-		returns a link to a user's profile and some information"""
-		try:
-			if not user.startswith("http://") and not user.startswith("https://"):
-				userName = user
-			else:
-				if user.startswith("https://"):
-					user = user.split("https://")[0]
-				userName = user.split("members.html?")[1]
+        returns a link to a user's profile and some information"""
+        try:
+            if not user.startswith("http://") and not user.startswith("https://"):
+                userName = user
+            else:
+                if user.startswith("https://"):
+                    user = user.split("https://")[0]
+                userName = user.split("members.html?")[1]
 
-			userData = json.loads(utils.web.getUrl("http://brilliant-minds.tk/members/{0}.json".format(userName)))
-			Awards = []
-			Rank = userData["rank"]
-			Status = ""
-			Links = []
+            userData = json.loads(utils.web.getUrl("http://brilliant-minds.tk/members/{0}.json".format(userName)))
+            Awards = []
+            awards = userData["awards"]
+            Rank = userData["rank"]
+            Status = ""
+            Links = []
 
-			for key, value in list(jsonObject["awards"].items()):
-				if str(value) == 0:
-					Value = "Badge"
-				elif str(value) == 1:
-					Value = "Standard"
-				elif str(value) == 2:
-					Value = "Bronze"
-				elif str(value) == 3:
-					Value = "Silver"
-				elif str(value) == 4:
-					Value = "Gold"
-				elif str(value) == 5:
-					Value = "Diamond"
-				Awards.append("{0}: {1}".format(key, Value))
+            for key, value in list(awards.items()):
+                if str(value) == 0:
+                    Value = "Badge"
+                elif str(value) == 1:
+                    Value = "Standard"
+                elif str(value) == 2:
+                    Value = "Bronze"
+                elif str(value) == 3:
+                    Value = "Silver"
+                elif str(value) == 4:
+                    Value = "Gold"
+                elif str(value) == 5:
+                    Value = "Diamond"
+                Awards.append("{0}: {1}".format(key, Value))
 
-			for key, value in jsonObject["links"]:
-				Links.append("{0}: {1}".format(key, value))
-			Links = ", ".join(Links)
-			Awards = ", ".join(Awards)
+            for key, value in userData["links"]:
+                Links.append("{0}: {1}".format(key, value))
+            Links = ", ".join(Links)
+            Awards = ", ".join(Awards)
 
-			if userData["voucher"]:
-				Status = "This member has a voucher"
-				if userData["safe"] == 1:
-					Status += " and is safe for the next IMC/IRC"
-				elif userData["safe"] == 2:
-					Status += " and is autosafe";
-			else:
-				if userData["safe"] == 1:
-					Status += "This member is safe for the next IMC/IRC"
-				elif userData["safe"] == 2:
-					Status += "This member is absolutely necessary to keep the group going and thus is autosafe"
-			
-			irc.reply("Member is: {0} {1} | {2} | http://brilliant-minds.tk/members.html?{3} | Awards {4} | {5}".format(Rank, userName, Safe, userName, each["Awards"], Links), prefixNick=False)
-		    self.log.info("UserInfo: Member {0} found".format(userName))
-		except Exception:
-			irc.reply("User {0} doesn't have any record in my database, sorry.".format(userName))
-			self.log.error("UserInfo: Member {0} not found".format(userName))
-		finally:
-			return None
+            if userData["voucher"]:
+                Status = "This member has a voucher"
+                if userData["safe"] == 1:
+                    Status += " and is safe for the next IMC/IRC"
+                elif userData["safe"] == 2:
+                    Status += " and is autosafe";
+            else:
+                if userData["safe"] == 1:
+                    Status += "This member is safe for the next IMC/IRC"
+                elif userData["safe"] == 2:
+                    Status += "This member is absolutely necessary to keep the group going and thus is autosafe"
+	
+            irc.reply("Member is: {0} {1} | {2} | http://brilliant-minds.tk/members.html?{3} | Awards {4} | {5}".format(Rank, userName, Safe, userName, Awards, Links), prefixNick=False)
+            self.log.info("UserInfo: Member {0} found".format(userName))
+        except Exception:
+            irc.reply("User {0} doesn't have any record in my database, sorry.".format(userName))
+            self.log.error("UserInfo: Member {0} not found".format(userName))
+        finally:
+            return None
 
 Class = UserInfo
 
