wolfy1339/Limnoria-Plugins

commit 602d88dda92ac2eb89761bf5b52c68d728f6f377

Use decorator syntax instead of wrapper

CustOps/plugin.py
@@ -26,6 +26,7 @@ class CustOps(callbacks.Plugin):
         i = random.random()
     del i
 
+    @wrap(['op', ('haveOp', 'remove a user from the channel'), 'nickInChannel', additional('text')])
     def ninja(self, irc, msg, args, channel, user, reason):
         """<user> [reason]
 
@@ -38,9 +39,7 @@ class CustOps(callbacks.Plugin):
         irc.queueMsg(ircmsgs.IrcMsg('REMOVE {0} {1} :{2}'.format(channel, user,
                                                                  reason)))
 
-    ninja = wrap(ninja, ['op', ('haveOp', 'remove a user from the channel'),
-                         'nickInChannel', additional('text')])
-
+    @wrap(['op', ('haveOp', 'Evict a user to #powder-social'), 'nickInChannel', optional('anything')])
     def social(self, irc, msg, args, channel, user, junk):
         """[#powder] <user>
 
@@ -69,9 +68,7 @@ class CustOps(callbacks.Plugin):
 
         schedule.addEvent(f, expires)
 
-    social = wrap(social, ['op', ('haveOp', 'Evict a user to #powder-social'),
-                           'nickInChannel', optional('anything')])
-
+    @wrap(['op', ('haveOp', 'Quiet a user'), 'nickInChannel', optional('int'), optional('text')])
     def stab(self, irc, msg, args, channel, user, timer, reason):
         """<user> [seconds] [reason (ignored)]
 
@@ -109,9 +106,7 @@ class CustOps(callbacks.Plugin):
         schedule.addEvent(f, expires)
         irc.noReply()
 
-    stab = wrap(stab, ['op', ('haveOp', 'Quiet a user'),
-                       'nickInChannel', optional('int'), optional('text')])
-
+    @wrap(['op', ('haveOp', 'Unquiet a user'), 'nickInChannel', optional('int'), optional('text')])
     def unstab(self, irc, msg, args, channel, user):
         """<user>
 
@@ -122,9 +117,6 @@ class CustOps(callbacks.Plugin):
             '*', '*', ircutils.hostFromHostmask(hmask))
         irc.queueMsg(ircmsgs.IrcMsg(
             'MODE {0} -q {1}'.format(channel, hostmask)))
-
-    unstab = wrap(unstab, ['op', ('haveOp', 'Set user modes'),
-                           'nickInChannel'])
 
 Class = CustOps
 

General/plugin.py
@@ -62,6 +62,7 @@ class General(callbacks.PluginRegexp):
 
 # Commands
 
+    @wrap(['hostmask'])
     def banmask(self, irc, msg, args, hostmask):
         """<nick|hostmask>
 
@@ -87,8 +88,7 @@ class General(callbacks.PluginRegexp):
                               'BANMASK: *!*@{0} returned for {1}'.format(
                                 hostmask, msg.nick))
 
-    banmask = wrap(banmask, ['hostmask'])
-
+    @wrap
     def me(self, irc, msg, args):
         """<no arguments>
 
@@ -96,8 +96,7 @@ class General(callbacks.PluginRegexp):
 
         irc.reply(msg.nick)
 
-    me = wrap(me, [])
-
+    @wrap(['int', optional('int'), optional('int')])
     def rand(self, irc, msg, args, min, max, num):
         """[min] <max> [amount]
 
@@ -137,8 +136,7 @@ class General(callbacks.PluginRegexp):
             x += 1
         irc.reply(output)
 
-    rand = wrap(rand, ['int', optional('int'), optional('int')])
-
+    @wrap(['text'])
     def geoip(self, irc, msg, args, ohostmask):
         """<IPv4 Address>
 
@@ -230,8 +228,7 @@ class General(callbacks.PluginRegexp):
             'The provider is {6}').format(
                 hostmask, city, country, tinyurlLink, tz, to, provider))
 
-    geoip = wrap(geoip, ['text'])
-
+    @wrap(['something', additional('text')])
     def bug(self, irc, msg, args, cmd, txt):
         """<plugin> [details of bug]
 
@@ -246,8 +243,7 @@ class General(callbacks.PluginRegexp):
                         self.ownerNick, cmd, msg.nick, txt))
         irc.replySuccess('Bug reported.')
 
-    bug = wrap(bug, ['something', additional('text')])
-
+    @wrap([optional("nick")])
     def kicked(self, irc, msg, args, nick):
         """[user]
 
@@ -276,8 +272,7 @@ class General(callbacks.PluginRegexp):
         except:
             irc.reply('{0} hasn"t been kicked it seems.'.format(nick))
 
-    kicked = wrap(kicked, [optional("nick")])
-
+    @wrap(['op', 'nickInChannel', optional('float')])
     def annoy(self, irc, msg, args, channel, nick, mins):
         """[channel] <nick> [mins]
 
@@ -308,8 +303,7 @@ class General(callbacks.PluginRegexp):
             'ANNOY: Annoying {0} for {1} minutes'.format(nick, mins))
         self.annoyUser += [nick.lower()]
 
-    annoy = wrap(annoy, ['op', 'nickInChannel', optional('float')])
-
+    @wrap(['op', 'nick'])
     def unannoy(self, irc, msg, args, channel, nick):
         """[channel] <nick>
 
@@ -322,8 +316,7 @@ class General(callbacks.PluginRegexp):
             irc.error('That user isn\'t being annoyed.')
             return 0
 
-    unannoy = wrap(unannoy, ['op', 'nick'])
-
+    @wrap(["something"])
     def justme(self, irc, msg, args, url):
         """<url>
 
@@ -349,8 +342,7 @@ class General(callbacks.PluginRegexp):
         else:
             irc.error("Check URL and try again")
 
-    justme = wrap(justme, ["something"])
-
+    @wrap(["op", ("haveOp", "Kick a user"), "something", "something", optional("text")])
     def multikick(self, irc, msg, args, channel, nick, num, message):
         """<nick> <num> [message]
 
@@ -380,10 +372,6 @@ class General(callbacks.PluginRegexp):
         irc.reply(
                 "Kicking anyone with {0} in their nick {1} times.".format(
                     nick, num), private=True)
-
-    multikick = wrap(multikick, [
-            "op", ("haveOp", "Kick a user"), "something", "something",
-            optional("text")])
 
 # RegExps
 
@@ -506,6 +494,7 @@ class General(callbacks.PluginRegexp):
             "&")
         irc.reply("Youtube video is '{0}'".format(data), prefixNick=nickPrefix)
 
+    @wrap(["text"])
     def youtube(self, irc, msg, args, url):
         """<url>
 
@@ -513,8 +502,7 @@ class General(callbacks.PluginRegexp):
 
         self._ytinfo(irc, url, True)
 
-    youtube = wrap(youtube, ["text"])
-
+    @urlSnarfer
     def ytSnarfer(self, irc, msg, match):
         r""".*(youtube[.]com/.+v=[0-9A-z\-_]{11}).*"""
 
@@ -525,8 +513,7 @@ class General(callbacks.PluginRegexp):
         else:
             return
 
-    ytSnarfer = urlSnarfer(ytSnarfer)
-
+    @urlSnarfer
     def capsKick(self, irc, msg, match):
         r""".+"""
 
@@ -585,8 +572,7 @@ class General(callbacks.PluginRegexp):
             del kd
             irc.queueMsg(ircmsgs.kick(msg.args[0], msg.nick, reason))
 
-    capsKick = urlSnarfer(capsKick)
-
+    @urlSnarfer
     def pasteSnarfer(self, irc, msg, match):
         r"""http://pastebin[.]com/[A-Za-z0-9]{8}"""
 
@@ -619,8 +605,7 @@ class General(callbacks.PluginRegexp):
                     paste['size'], paste['expires']),
                 prefixNick=False)
 
-    pasteSnarfer = urlSnarfer(pasteSnarfer)
-
+    @urlSnarfer
     def selfCorrect(self, irc, msg, match):
         r"""^s[/].*[/].*$"""
 
@@ -670,8 +655,7 @@ class General(callbacks.PluginRegexp):
         else:
             return
 
-    selfCorrect = urlSnarfer(selfCorrect)
-
+    @urlSnarfer
     def userCorrect(self, irc, msg, match):
         r"""^u[/].*[/].*[/].*$"""
 
@@ -734,8 +718,7 @@ class General(callbacks.PluginRegexp):
         else:
             return
 
-    userCorrect = urlSnarfer(userCorrect)
-
+    @urlSnarfer
     def saveLast(self, irc, msg, match):
         r""".+"""
 
@@ -791,8 +774,7 @@ class General(callbacks.PluginRegexp):
             self.buffer[channel].pop(self.buffsize)
         return 1
 
-    saveLast = urlSnarfer(saveLast)
-
+    @wrap([optional("text")])
     def pop(self, irc, msg, args, text):
         """
 
@@ -812,8 +794,6 @@ class General(callbacks.PluginRegexp):
                 irc.reply('pops {}'.format(text), action=True)
             else:
                 irc.reply('goes pop!', action=True)
-
-    pop = wrap(pop, [optional("text")])
 
 # Utilities
     def _sendMsg(self, irc, msg):

Hash/plugin.py
@@ -48,6 +48,7 @@ class Hash(callbacks.Plugin):
     """An assortment of hashing functions"""
     threaded = True
 
+    @wrap(['text'])
     def md5(self, irc, msg, args, text):
         """<text>
 
@@ -55,8 +56,7 @@ class Hash(callbacks.Plugin):
         """
         irc.reply(hashlib.md5(text).hexdigest())
 
-    md5 = wrap(md5, ['text'])
-
+    @wrap(['text'])
     def sha1(self, irc, msg, args, text):
         """<text>
 
@@ -64,8 +64,7 @@ class Hash(callbacks.Plugin):
         """
         irc.reply(hashlib.sha1(text).hexdigest())
 
-    sha1 = wrap(sha1, ['text'])
-
+    @wrap(['text'])
     def sha224(self, irc, msg, args, text):
         """<text>
 
@@ -73,8 +72,7 @@ class Hash(callbacks.Plugin):
         """
         irc.reply(hashlib.sha224(text).hexdigest())
 
-    sha224 = wrap(sha224, ['text'])
-
+    @wrap(['text'])
     def sha256(self, irc, msg, args, text):
         """<text>
 
@@ -82,8 +80,7 @@ class Hash(callbacks.Plugin):
         """
         irc.reply(hashlib.sha256(text).hexdigest())
 
-    sha256 = wrap(sha256, ['text'])
-
+    @wrap(['text'])
     def sha384(self, irc, msg, args, text):
         """<text>
 
@@ -91,16 +88,13 @@ class Hash(callbacks.Plugin):
         """
         irc.reply(hashlib.sha384(text).hexdigest())
 
-    sha384 = wrap(sha384, ['text'])
-
+    @wrap(['text'])
     def sha512(self, irc, msg, args, text):
         """<text>
 
         Creates a sha-512 hash on <text>
         """
         irc.reply(hashlib.sha512(text).hexdigest())
-
-    sha512 = wrap(sha512, ['text'])
 
 Class = Hash
 

Powder/plugin.py
@@ -54,6 +54,7 @@ class Powder(callbacks.PluginRegexp):
         self._getSaveInfo(irc, ID)
     browse = wrap(browse, ['somethingWithoutSpaces'])
 
+    @urlSnarfer
     def powderSnarfer(self, irc, msg, match):
         r"http://powdertoy.co.uk/Browse/View.html\?ID=([0-9]+)|^[~]([0-9]+)|http://tpt.io/~([0-9]+)|http://powdertoy.co.uk/~([0-9]+)"
         ID = match.group(1) or match.group(
@@ -67,8 +68,6 @@ class Powder(callbacks.PluginRegexp):
             self._getSaveInfo(irc, ID)
         else:
             return
-
-    powderSnarfer = urlSnarfer(powderSnarfer)
 
     def _getSaveInfo(self, irc, ID):
         if 'http://' in ID:
@@ -93,6 +92,7 @@ class Powder(callbacks.PluginRegexp):
                 saveMsg += ' ' + url
         irc.reply(saveMsg, prefixNick=False)
 
+    @wrap
     def frontpage(self, irc, msg, args):
         """
 
@@ -113,15 +113,14 @@ class Powder(callbacks.PluginRegexp):
                 continue
             outMsg = '{0} -- '.format(outMsg)
 
-    frontpage = wrap(frontpage)
-
+    @wrap(['something'])
     def forum(self, irc, msg, num):
         """
 
         Returns information on a forum post."""
         self._getPostDetails(irc, msg, num)
-    forum = wrap(forum, ['something'])
 
+    @urlSnarfer
     def forumSnarfer(self, irc, msg, match):
         r"http://powdertoy[.]co[.]uk/Discussions/Thread/View[.]html[?]Thread=([0-9]+)|http://tpt.io/:([0-9]+)|^:([0-9]+)"
         threadNum = match.group(1) or match.group(2) or match.group(3)
@@ -129,8 +128,6 @@ class Powder(callbacks.PluginRegexp):
             self._getPostDetails(irc, msg, threadNum)
         else:
             return
-
-    forumSnarfer = urlSnarfer(forumSnarfer)
 
     def _getPostDetails(self, irc, msg, threadNum):
         jsonFile = utils.web.getUrl('http://powdertoy.co.uk/Discussions/Thread/View.json?Thread={0}'.format(threadNum))
@@ -153,6 +150,7 @@ class Powder(callbacks.PluginRegexp):
             'FORUMSNARF: Thread %s found. %s in the %s section' %
             (threadNum, tp['Title'], cg['Name']))
 
+    @wrap(['something'])
     def profile(self, irc, msg, args, user):
         """<username|ID>
 
@@ -205,8 +203,7 @@ class Powder(callbacks.PluginRegexp):
                 irc.reply(
                     'User or ID doesn\'t exist - or Xeno screwed it again... {0}'.format(e))
 
-    profile = wrap(profile, ['something'])
-
+    @wrap
     def randomsave(self, irc, msg, args):
         """
 
@@ -223,8 +220,8 @@ class Powder(callbacks.PluginRegexp):
                 found = True
 
         self._getSaveInfo(irc, saveID, 0)
-    randomsave = wrap(randomsave)
 
+    @wrap
     def comic(self, irc, msg, args):
         """
 
@@ -251,7 +248,7 @@ class Powder(callbacks.PluginRegexp):
                     filename))
         except:
             irc.error('Comic checker is broken, use $bug comic')
-    comic = wrap(comic)
+
 Class = Powder
 
 # vim:set shiftwidth=4 softtabstop=4 expandtab textwidth=79:

Rpg/plugin.py
@@ -61,6 +61,7 @@ class Rpg(callbacks.Plugin):
             self.filepath = "/home/wolfy1339/BMNBot-New/plugins/Rpg/"
 
         # Game Commands
+        @wrap
         def reloadData(self, irc, msg, args):
             if not ircdb.users.getUser(msg.prefix)._checkCapability("admin"):
                 irc.error("Only people with \"Admin\" can do that.")
@@ -73,8 +74,8 @@ class Rpg(callbacks.Plugin):
                     self.monsterData = json.load(f)
                     self._getItemsFile()
                     irc.replySuccess()
-        reloaddata = wrap(reloadData)
 
+        @wrap([optional("somethingWithoutSpaces"), optional("somethingWithoutSpaces")])
         def genMap(self, irc, msg, args, width, height):
             if not ircdb.users.getUser(msg.prefix)._checkCapability("owner"):
                 irc.error("Only people with \"Admin\" can do that.")
@@ -157,10 +158,7 @@ class Rpg(callbacks.Plugin):
 # submit  =  utils.web.getUrl(self.serverUrl+"?m = %s&w = %i&h = %&hm =
 # %i&hy = %i
 
-        genmap = wrap(genMap,
-                      [optional("somethingWithoutSpaces"),
-                       optional("somethingWithoutSpaces")])
-
+        @wrap
         def stats(self, irc, msg, args):
             player = self._checkPlayer(irc, msg)
             playerData = self.playerData[player]
@@ -194,8 +192,8 @@ class Rpg(callbacks.Plugin):
                        "attacks. Your Luck rating is {15}. "
                        "You have died {16} times."
                        ).format(player, level, exp, next, hp, mhp, baseAtk, totalAtk, weapon, baseDef, totalDef, helmet, armour, shield, block, luck, deaths))
-        stats = wrap(stats)
 
+        @wrap
         def new(self, irc, msg, args):
             player = self._checkPlayer(irc, msg, 1)
             playerData = self.playerData
@@ -231,8 +229,8 @@ class Rpg(callbacks.Plugin):
             self._sendDbg(irc, player + " has been reset/created")
             self._savePlayerData(playerData)
             self.rpgStats(irc, msg, args)
-        new = wrap(new)
 
+        @wrap
         def location(self, irc, msg, args):
             player = self._checkPlayer(irc, msg)
             location = self.playerData[player]["Loc"]
@@ -251,8 +249,8 @@ class Rpg(callbacks.Plugin):
             irc.reply((
                 "You are located at ({0}, {1})."
                 " Home is at ({2}, {3})").format(x, y, homeX, homeY))
-        loc = wrap(location)
 
+        @wrap
         def ViewArea(self, irc, msg, args):
             player = self._checkPlayer(irc, msg)
             location = self.playerData[player]["Loc"]
@@ -293,8 +291,8 @@ class Rpg(callbacks.Plugin):
                            area[6],
                            area[7])
                       )
-        viewArea = wrap(ViewArea)
 
+        @wrap
         def forceBattle(self, irc, msg, args):
             player = self._checkPlayer(irc, msg)
             if self.playerData[player]["force"]:
@@ -307,8 +305,8 @@ class Rpg(callbacks.Plugin):
                 text = "will enter a monster battle on their next turn."
                 irc.reply("{0} {1}".format(player.capitalize(), text),
                           prefixNick=False)
-        forceBattle = wrap(forceBattle)
 
+        @wrap(["somethingWithoutSpaces", optional("int")])
         def move(self, irc, msg, args, direction, number):
             player = self._checkPlayer(irc, msg)
             playerData = self.playerData
@@ -415,8 +413,6 @@ class Rpg(callbacks.Plugin):
                     self._notice(irc, msg.nick, text)
                     self._savePlayerData(playerData)
                 x += 1
-
-        move = wrap(move, ["somethingWithoutSpaces", optional("int")])
 
     #  Engine functions
 

UserInfo/plugin.py
@@ -48,6 +48,7 @@ class UserInfo(callbacks.Plugin):
     threaded = True
 
     @internationalizeDocstring
+    @wrap([optional('somethingWithoutSpaces')])
     def records(self, irc, msg, args, user):
         """[<memberName>]
 
@@ -59,8 +60,8 @@ class UserInfo(callbacks.Plugin):
             self._getMemberInfo(irc, user)
         else:
             self._getMemberInfo(irc, msg.nick)
-    records = wrap(records, [optional('somethingWithoutSpaces')])
 
+    @urlSnarfer
     def UserInfoSnarfer(self, irc, msg, args, match):
         r"http://brilliant-minds.tk/members.html\?([\w-]+)|@([\w-]+)"
         name = match.group(1) or match.group(2)
@@ -76,9 +77,8 @@ class UserInfo(callbacks.Plugin):
         else:
             return
 
-    UserInfoSnarfer = urlSnarfer(UserInfoSnarfer)
-
     @internationalizeDocstring
+    @wrap
     def members(self, irc, msg, args):
         """No arguments
 
@@ -106,7 +106,6 @@ class UserInfo(callbacks.Plugin):
             irc.reply(message, nickPrefix=False)
         else:
             irc.reply(message, private=True)
-    members = wrap(members)
 
     def _getMemberInfo(self, irc, user):
         if (not user.startswith('http://') and
@@ -115,7 +114,7 @@ class UserInfo(callbacks.Plugin):
         else:
             userName = user.split('members.html?')[1]
 
-        url = 'http://brilliant-minds.tk/members/{0}.json'.format(userName)
+        url = 'http://brilliant-minds.github.io/members/{0}.json'.format(userName)
         try:
             jsonFile = utils.web.getUrl(url)
         except utils.web.Error:

commit 625f04660b38669ef18773c7185be8ef0c4cee79

Fix error introduced in 602d88dda92ac2eb89761bf5b52c68d728f6f377
There were extra arguments than what was defined in the function

CustOps/plugin.py
@@ -106,7 +106,7 @@ class CustOps(callbacks.Plugin):
         schedule.addEvent(f, expires)
         irc.noReply()
 
-    @wrap(['op', ('haveOp', 'Unquiet a user'), 'nickInChannel', optional('int'), optional('text')])
+    @wrap(['op', ('haveOp', 'Unquiet a user'), 'nickInChannel'])
     def unstab(self, irc, msg, args, channel, user):
         """<user>
 